ba49e49ffaac7d1bd40dda7abc28c0e19b0ce892
pcstrategyandopsco <120511573+pcstrategyandopsco@users.noreply.github.com>
2023-02-10 18:10:38 +1300

    Add files via upload

--- temp.js
+++ temp.js
@@ -1,4 +1,4 @@
-Highcharts.chart('container2', {
+Highcharts.chart('container', {
   chart: {
     reflow: true
   },
@@ -41,6 +41,330 @@ Highcharts.chart('container2', {
     {
       group: "group",
       data: [
+        {
+          plot_date: "2009 June",
+          "6 months": 65.2316,
+          y: 65.2316,
+          name: "2009 June"
+        },
+        {
+          plot_date: "2009 December",
+          "6 months": 69.1282,
+          y: 69.1282,
+          name: "2009 December"
+        },
+        {
+          plot_date: "2010 March",
+          "6 months": 66.9013,
+          y: 66.9013,
+          name: "2010 March"
+        },
+        {
+          plot_date: "2010 June",
+          "6 months": 66.767,
+          y: 66.767,
+          name: "2010 June"
+        },
+        {
+          plot_date: "2010 September",
+          "6 months": 68.6134,
+          y: 68.6134,
+          name: "2010 September"
+        },
+        {
+          plot_date: "2010 December",
+          "6 months": 68.9664,
+          y: 68.9664,
+          name: "2010 December"
+        },
+        {
+          plot_date: "2011 March",
+          "6 months": 67.2613,
+          y: 67.2613,
+          name: "2011 March"
+        },
+        {
+          plot_date: "2011 June",
+          "6 months": 69.1408,
+          y: 69.1408,
+          name: "2011 June"
+        },
+        {
+          plot_date: "2011 September",
+          "6 months": 70.1741,
+          y: 70.1741,
+          name: "2011 September"
+        },
+        {
+          plot_date: "2011 December",
+          "6 months": 72.3301,
+          y: 72.3301,
+          name: "2011 December"
+        },
+        {
+          plot_date: "2012 March",
+          "6 months": 71.4744,
+          y: 71.4744,
+          name: "2012 March"
+        },
+        {
+          plot_date: "2012 June",
+          "6 months": 72.3821,
+          y: 72.3821,
+          name: "2012 June"
+        },
+        {
+          plot_date: "2012 September",
+          "6 months": 73.9147,
+          y: 73.9147,
+          name: "2012 September"
+        },
+        {
+          plot_date: "2012 December",
+          "6 months": 75.695,
+          y: 75.695,
+          name: "2012 December"
+        },
+        {
+          plot_date: "2013 March",
+          "6 months": 75.6734,
+          y: 75.6734,
+          name: "2013 March"
+        },
+        {
+          plot_date: "2013 June",
+          "6 months": 76.7875,
+          y: 76.7875,
+          name: "2013 June"
+        },
+        {
+          plot_date: "2013 September",
+          "6 months": 76.3974,
+          y: 76.3974,
+          name: "2013 September"
+        },
+        {
+          plot_date: "2013 December",
+          "6 months": 77.4135,
+          y: 77.4135,
+          name: "2013 December"
+        },
+        {
+          plot_date: "2014 March",
+          "6 months": 76.157,
+          y: 76.157,
+          name: "2014 March"
+        },
+        {
+          plot_date: "2014 June",
+          "6 months": 78.5776,
+          y: 78.5776,
+          name: "2014 June"
+        },
+        {
+          plot_date: "2014 September",
+          "6 months": 78.994,
+          y: 78.994,
+          name: "2014 September"
+        },
+        {
+          plot_date: "2014 December",
+          "6 months": 79.8226,
+          y: 79.8226,
+          name: "2014 December"
+        },
+        {
+          plot_date: "2015 March",
+          "6 months": 79.2661,
+          y: 79.2661,
+          name: "2015 March"
+        },
+        {
+          plot_date: "2015 June",
+          "6 months": 79.8316,
+          y: 79.8316,
+          name: "2015 June"
+        },
+        {
+          plot_date: "2015 September",
+          "6 months": 80.5693,
+          y: 80.5693,
+          name: "2015 September"
+        },
+        {
+          plot_date: "2015 December",
+          "6 months": 81.9879,
+          y: 81.9879,
+          name: "2015 December"
+        },
+        {
+          plot_date: "2016 March",
+          "6 months": 80.2899,
+          y: 80.2899,
+          name: "2016 March"
+        },
+        {
+          plot_date: "2016 June",
+          "6 months": 80.6843,
+          y: 80.6843,
+          name: "2016 June"
+        },
+        {
+          plot_date: "2016 September",
+          "6 months": 81.1206,
+          y: 81.1206,
+          name: "2016 September"
+        },
+        {
+          plot_date: "2016 December",
+          "6 months": 80.8946,
+          y: 80.8946,
+          name: "2016 December"
+        },
+        {
+          plot_date: "2017 March",
+          "6 months": 78.7681,
+          y: 78.7681,
+          name: "2017 March"
+        },
+        {
+          plot_date: "2017 June",
+          "6 months": 79.662,
+          y: 79.662,
+          name: "2017 June"
+        },
+        {
+          plot_date: "2017 September",
+          "6 months": 78.5832,
+          y: 78.5832,
+          name: "2017 September"
+        },
+        {
+          plot_date: "2017 December",
+          "6 months": 78.0141,
+          y: 78.0141,
+          name: "2017 December"
+        },
+        {
+          plot_date: "2018 March",
+          "6 months": 77.6075,
+          y: 77.6075,
+          name: "2018 March"
+        },
+        {
+          plot_date: "2018 June",
+          "6 months": 77.6629,
+          y: 77.6629,
+          name: "2018 June"
+        },
+        {
+          plot_date: "2018 September",
+          "6 months": 77.4954,
+          y: 77.4954,
+          name: "2018 September"
+        },
+        {
+          plot_date: "2018 December",
+          "6 months": 77.552,
+          y: 77.552,
+          name: "2018 December"
+        },
+        {
+          plot_date: "2019 March",
+          "6 months": 77.4432,
+          y: 77.4432,
+          name: "2019 March"
+        },
+        {
+          plot_date: "2019 June",
+          "6 months": 78.6174,
+          y: 78.6174,
+          name: "2019 June"
+        },
+        {
+          plot_date: "2019 September",
+          "6 months": 78.675,
+          y: 78.675,
+          name: "2019 September"
+        },
+        {
+          plot_date: "2019 December",
+          "6 months": 80.1846,
+          y: 80.1846,
+          name: "2019 December"
+        },
+        {
+          plot_date: "2020 January",
+          "6 months": 77.063,
+          y: 77.063,
+          name: "2020 January"
+        },
+        {
+          plot_date: "2020 February",
+          "6 months": 80.3764,
+          y: 80.3764,
+          name: "2020 February"
+        },
+        {
+          plot_date: "2020 March",
+          "6 months": 80.4917,
+          y: 80.4917,
+          name: "2020 March"
+        },
+        {
+          plot_date: "2020 April",
+          "6 months": 75.5593,
+          y: 75.5593,
+          name: "2020 April"
+        },
+        {
+          plot_date: "2020 May",
+          "6 months": 75.4978,
+          y: 75.4978,
+          name: "2020 May"
+        },
+        {
+          plot_date: "2020 June",
+          "6 months": 77.4254,
+          y: 77.4254,
+          name: "2020 June"
+        },
+        {
+          plot_date: "2020 July",
+          "6 months": 78.5218,
+          y: 78.5218,
+          name: "2020 July"
+        },
+        {
+          plot_date: "2020 August",
+          "6 months": 78.2076,
+          y: 78.2076,
+          name: "2020 August"
+        },
+        {
+          plot_date: "2020 September",
+          "6 months": 78.7609,
+          y: 78.7609,
+          name: "2020 September"
+        },
+        {
+          plot_date: "2020 October",
+          "6 months": 78.5015,
+          y: 78.5015,
+          name: "2020 October"
+        },
+        {
+          plot_date: "2020 November",
+          "6 months": 77.0791,
+          y: 77.0791,
+          name: "2020 November"
+        },
+        {
+          plot_date: "2020 December",
+          "6 months": 75.9403,
+          y: 75.9403,
+          name: "2020 December"
+        },
         {
           plot_date: "2021 January",
           "6 months": 75.0555,
